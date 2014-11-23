b6bd88574d77a11a958562add52be8fe138cbbff
Khalid Ali <khalludi123@gmail.com>
2014-11-23 15:18:50 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,17 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    if (timeSinceObstacle > 2.0f) {
+        [self addObstacle];
+        
+        timeSinceObstacle = 0.0;
+    }
 }
 
 // put new methods here
